698c918cf39ca0cf117fef5053d8727976f88c7e
dinhthuw <90031105+dinhthuw@users.noreply.github.com>
2025-04-01 18:36:14 +0700

    Update booksApi.js

--- src/redux/features/books/booksApi.js
+++ src/redux/features/books/booksApi.js
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 export const booksApi = createApi({
     reducerPath: 'booksApi',
     baseQuery: fetchBaseQuery({ 
-        baseUrl: 'https://backend-8zha.onrender.com/api',
+        baseUrl: 'https://backend-e339.onrender.com',
         prepareHeaders: (headers, { getState }) => {
             const token = localStorage.getItem('token')
             if (token) {
