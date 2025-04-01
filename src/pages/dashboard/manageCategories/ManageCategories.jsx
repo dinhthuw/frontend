ec2768f33a29b9012a53ac8db45ebcc327938c05
dinhthuw <90031105+dinhthuw@users.noreply.github.com>
2025-04-01 17:46:10 +0700

    Update ManageCategories.jsx

--- src/pages/dashboard/manageCategories/ManageCategories.jsx
+++ src/pages/dashboard/manageCategories/ManageCategories.jsx
@@ -13,7 +13,7 @@ const ManageCategories = () => {
         description: ''
     });
 
-    const API_URL = import.meta.env.VITE_API_URL || 'https://backend-bb1s.onrender.com';
+    const API_URL = import.meta.env.VITE_API_URL || 'https://backend-8zha.onrender.com';
 
     useEffect(() => {
         fetchCategories();
